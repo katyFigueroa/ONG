4e04c4a24fbd627ce496f10672b21a7679ee7678
SIS-LAP-000\Sis. Toshiba <katyfigueroam.2@gmail.com>
2016-11-10 09:34:58 -0500

    proceso

---
+++ js/mostrar.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){ $(".capa1").hover(function(){ $(".mensaje1").show();  }, function(){ $(".mensaje1").hide(); e.preventDefault()}); });
+$(document).ready(function(){ $(".capa2").hover(function(){ $(".mensaje2").show();  }, function(){ $(".mensaje2").hide(); }); });
+$(document).ready(function(){ $(".capa3").hover(function(){ $(".mensaje3").show();  }, function(){ $(".mensaje3").hide(); }); });
+$(document).ready(function(){ $(".capa4").hover(function(){ $(".mensaje4").show();  }, function(){ $(".mensaje4").hide(); }); });
+$(document).ready(function(){ $(".capa5").hover(function(){ $(".mensaje5").show();  }, function(){ $(".mensaje5").hide(); }); });
+$(document).ready(function(){ $(".capa6").hover(function(){ $(".mensaje6").show();  }, function(){ $(".mensaje6").hide(); }); });
+$(document).ready(function(){ $(".capa7").hover(function(){ $(".mensaje7").show();  }, function(){ $(".mensaje7").hide(); }); });
+$(document).ready(function(){ $(".capa8").hover(function(){ $(".mensaje8").show();  }, function(){ $(".mensaje8").hide(); }); });
+
+
+$(document).ready(function(){$(".capa1").hover(function(){$(".capa").hide();}, function(){$(".capa").show();}); });
+$(document).ready(function(){$(".capa2").hover(function(){$(".capa").hide();}, function(){$(".capa").show();});});
+$(document).ready(function(){$(".capa3").hover(function(){$(".capa").hide();}, function(){$(".capa").show();});});
+$(document).ready(function(){$(".capa4").hover(function(){$(".capa").hide();}, function(){$(".capa").show();});});
+$(document).ready(function(){$(".capa5").hover(function(){$(".capa").hide();}, function(){$(".capa").show();});});
+$(document).ready(function(){$(".capa6").hover(function(){$(".capa").hide();}, function(){$(".capa").show();});});
+$(document).ready(function(){$(".capa7").hover(function(){$(".capa").hide();}, function(){$(".capa").show();});});
+$(document).ready(function(){$(".capa8").hover(function(){$(".capa").hide();}, function(){$(".capa").show();});});
