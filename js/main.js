79de9ca85a1bc9a952771016f912596867059a34
SIS-LAP-000\Sis. Toshiba <katyfigueroam.2@gmail.com>
2016-11-30 13:22:22 -0500

    linkeados

--- js/main.js
+++ js/main.js
@@ -1,36 +1,8 @@
 
-// var canvas, stage, exportRoot;
-// function init() {
-// 	// --- write your JS code here ---
-	
-// 	canvas = document.getElementById("canvas");
-// 	images = images||{};
-
-// 	var loader = new createjs.LoadQueue(false);
-// 	loader.addEventListener("fileload", handleFileLoad);
-// 	loader.addEventListener("complete", handleComplete);
-// 	loader.loadManifest(lib.properties.manifest);
-// }
-
-// function handleFileLoad(evt) {
-// 	if (evt.item.type == "image") { images[evt.item.id] = evt.result; }
-// }
-
-// function handleComplete(evt) {
-// 	exportRoot = new lib.Sintítulo2();
-
-// 	stage = new createjs.Stage(canvas);
-// 	stage.addChild(exportRoot);
-// 	stage.update();
-
-// 	createjs.Ticker.setFPS(lib.properties.fps);
-// 	createjs.Ticker.addEventListener("tick", stage);
-// }
-
-
 $(document).ready(function() {
   $('#media').carousel({
     pause: true,
     interval: 1,
   });
 });
+
